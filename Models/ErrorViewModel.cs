390a5b8820a25cd2ff3ca28acbfe2e22689e72dd
Leticia Passer <leticia.passer@pump-control.com.ar>
2024-12-16 16:55:44 -0300

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Prog_PumpControl.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
